703f3b9c03b907e932bd1d61becc88980e6e02c1
Aleksandar Petrov <sasho1320@gmail.com>
2024-08-21 09:07:59 +0300

    update .gitignore file

--- main.go
+++ main.go
@@ -32,6 +32,9 @@ func createEvent(context *gin.Context) {
 
 	event.ID = 1
 	event.UserID = 1
+
+	event.Save()
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
 
 }
