4acb7677c0ec29168a6507debccaa8cc8dfec457
Aleksandar Petrov <sasho1320@gmail.com>
2024-08-31 07:59:31 +0300

    adding authentication middleware

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"example.com/rest-api/models"
-	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -35,38 +34,25 @@ func getEvent(context *gin.Context) {
 }
 
 func createEvent(context *gin.Context) {
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized!"})
-		return
-	}
-
-	userID, err := utils.VerifyToken(token)
-
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not authorized!"})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse requiest data."})
 		return
 	}
 
+	userID := context.GetInt64("userID")
 	event.UserID = userID
 
 	err = event.Save()
+
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not create event."})
 		return
 	}
 
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created!", "event": event})
-
 }
 
 func updateEvent(context *gin.Context) {
